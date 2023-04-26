0b431920d2e9cb8d3ded54e16280e38b3b6e1c10
wangruohan1 <1594264255@qq.com>
2023-04-26 09:27:26 +0800

    第一次提交

---
+++ code/pytorch-classification-uncertainty-master/lenet.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+
+class LeNet(nn.Module):
+    def __init__(self, dropout=False):
+        super().__init__()
+        self.use_dropout = dropout
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=5)
+        self.conv2 = nn.Conv2d(20, 50, kernel_size=5)
+        self.fc1 = nn.Linear(20000, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 1))
+        x = F.relu(F.max_pool2d(self.conv2(x), 1))
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        if self.use_dropout:
+            x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
